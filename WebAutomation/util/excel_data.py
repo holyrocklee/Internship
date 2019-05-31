e1e068bac994d878dc41ccd3f24c9be58a3d76d4
ruiweixuan <rwxuan@che300.com>
2019-05-31 19:29:52 +0800

    update a little

--- WebAutomation/util/excel_data.py
+++ WebAutomation/util/excel_data.py
@@ -8,12 +8,21 @@ import requests as rq
 import pandas as pd
 import jsonpath
 import json
+import re
 
 df = pd.read_excel('D:\\cxli\\api\\excel_table\\发动机.xlsx')
 data=df.ix[:,['simple_detail','num']].values  #读所有行的simple_detail以及num列的值，这里需要嵌套列表
 #print(data)
-detail = data[0][0]
-print(detail)
-print(data[0][1])
+# detail = data[0][0]
+# print(detail)
+# number = data[0][1]
+PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市))([\u4e00-\u9fa5]{2,7}?(?:市|区|县|州)){0,1}([\u4e00-\u9fa5]{2,7}?(?:市|区|县)){0,1}'
+data_list = ['发动机', '引擎','换']
 
-
+for singledata in data:
+    print(singledata)
+    singledetail = singledata[0]
+    print(singledetail)
+    singlenum = singledata[1]
+    print(singlenum)
+    Regex = re.compile(r'')
