1e3a8541a0f61a9556ce311afcfe53f901afeb87
ruiweixuan <rwxuan@che300.com>
2019-06-03 17:58:55 +0800

    优化正则表达式

--- WebAutomation/util/excel_data.py
+++ WebAutomation/util/excel_data.py
@@ -1,28 +1,69 @@
 # -*- coding: utf-8 -*-
-# 开发人员   ：Rocklee
-# 开发时间   ：2019/5/31  18:45 
-# 文件名称   ：excel_data.PY
-# 开发工具   ：PyCharm
+# Author  ：Rocklee
+# Time  ：2019/5/31  20:24
 
-import requests as rq
 import pandas as pd
-import jsonpath
-import json
 import re
 
-df = pd.read_excel('D:\\cxli\\api\\excel_table\\发动机.xlsx')
-data=df.ix[:,['simple_detail','num']].values  #读所有行的simple_detail以及num列的值，这里需要嵌套列表
-#print(data)
-# detail = data[0][0]
-# print(detail)
-# number = data[0][1]
-PATTERN = r'([\u4e00-\u9fa5]{2,5}?(?:省|自治区|市))([\u4e00-\u9fa5]{2,7}?(?:市|区|县|州)){0,1}([\u4e00-\u9fa5]{2,7}?(?:市|区|县)){0,1}'
-data_list = ['发动机', '引擎','换']
+from util.excel_util import excelutil
 
-for singledata in data:
-    print(singledata)
-    singledetail = singledata[0]
-    print(singledetail)
-    singlenum = singledata[1]
-    print(singlenum)
-    Regex = re.compile(r'')
+
+def get_result():
+    df = pd.read_excel('D:\\cxli\\api\\input_files\\更换发动机.xls')  #待处理的文件；运行时只需要改变输入输出文件，正则表达式即可
+    # 将发动机excel文件插入第一行，表头分别为:描述详情的用simple_detail，描述序号的用num，其他表头无所谓
+    data=df.ix[:,['simple_detail','num']].values  #读所有行的simple_detail以及num列的值，这里需要嵌套列表
+    # detail = list(data[0][0])
+    # number = data[0][1]
+    PATTERN = r'([\u4e00-\u9fa5]*)(换)([\u4e00-\u9fa5]*)(发动机|引擎)([^;|；]*)'
+    i = -1
+    result = []
+    singlenumbers = []
+    detail = []
+    for singledata in data:
+        i = i+1
+        singledetail = singledata[0]
+        detail.append(singledetail)
+        singlenum = singledata[1]
+        print(singlenum)
+        singlenumbers.append(singlenum)
+        Regex = re.compile(PATTERN,re.S)
+        m = Regex.findall(singledetail)
+        if not m:
+            result.append("格式不对应")
+        else:
+            result.append(m)
+            result[i] = list(set(result[i]))
+    print(result)
+    return singlenumbers,result,detail
+
+def manage_result(resultA):
+    simplelist = []
+    for i in range(len(resultA)):
+        if resultA[i] == '格式不对应':
+            simplelist.append("格式不对应")
+        else:
+            if len(resultA[i])==1:
+                s = ''.join(resultA[i][0])
+                simplelist.append(s)
+            else:
+                doublelist = []
+                for j in range(len(resultA[i])):
+                    s = ''.join(resultA[i][j])
+                    doublelist.append(s)
+                simplelist.append(doublelist)
+    print(simplelist)
+    return simplelist
+
+def save_excel(singlenumbers,detail,simplelist):
+    # df = pd.DataFrame({'num': singlenumbers, 'details': detail, 'result': simplelist})
+    # df.to_excel('E:\\api\\final.xlsx')
+    # print("Done!")
+    util = excelutil('D:\\cxli\\api\\output_files\\发动机匹配1.xls','a', head=['num','details','result']) #注意先在本地建一个空白.xls文件
+    for singlenumber,singledetail,simpleresult in zip(singlenumbers,detail,simplelist):
+        util.write_nextline([singlenumber,singledetail,simpleresult], save=True)
+    print("Done!")
+
+if __name__ == '__main__':
+    singlenumbers,resultA,detail = get_result()
+    simplelist = manage_result(resultA)
+    save_excel(singlenumbers,detail,simplelist)
